cdef774ebd6271279af077f9ca95038d8edfaaed
joannewangrealtor <juan.wang@zohomail.com>
2025-06-19 19:19:01 -0400

    add broswer in index.js

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ library.add(fas);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>

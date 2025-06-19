da617c63dd1d2b6ce6a057be8081f7d724f93818
joannewangrealtor <juan.wang@zohomail.com>
2025-06-19 17:08:57 -0400

    add broswer in App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // src/App.js
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { LanguageProvider } from './components/LanguageContext'; // 引入 LanguageProvider
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { LanguageProvider } from './components/LanguageContext';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
-// import Listings from './components/Listings';
-// import Reviews from './components/Reviews';
 import Social from './components/Social';
 import Footer from './components/Footer';
 import FeaturedListings from './components/FeaturedListings';
-import Blog from './components/Blog'; 
+import Blog from './components/Blog';
 import BlogDetail from './components/BlogDetail';
 import HomeEvaluationForm from './components/HomeEvaluationForm';
 import SellingResources from './components/SellingResources';
@@ -20,29 +18,32 @@ import BuyingResources from './components/BuyingResources';
 function App() {
   return (
     <LanguageProvider>
-      <Header />
-      <Routes>
-        {/* Homepage route with all existing components */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <About />
-              <Services />
-              <Social />
-              <Footer />
-            </>
-          }
-        />
-        {/* Featured Listings route */}
-        <Route path="/featured-listings" element={<FeaturedListings />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogDetail />} />
-        <Route path="/home-evaluation" element={<HomeEvaluationForm />} />
-        <Route path="/selling-resources" element={<SellingResources />} />
-        <Route path='/buying-resources' element={<BuyingResources />} />
-      </Routes>
+      <BrowserRouter basename="/joanne">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <About />
+                <Services />
+                <Social />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/featured-listings" element={<FeaturedListings />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+          <Route path="/home-evaluation" element={<HomeEvaluationForm />} />
+          <Route path="/selling-resources" element={<SellingResources />} />
+          <Route path="/buying-resources" element={<BuyingResources />} />
+
+          {/* ✅ 修复：自动跳转未知路径（如初次访问）回首页 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </LanguageProvider>
   );
 }
